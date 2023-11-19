0faeb908853336dee5dc6b2eadbb8a01b7485fec
Berkay TİKENOĞLU <berkaytikenoglu@gmail.com>
2023-11-20 00:55:00 +0300

    Mağazaya yüklenme aşamasına gelindi

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Berkay TİKENOĞLU on 19.11.2023.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
